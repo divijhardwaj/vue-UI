d9d76ec55d0955ac7f6b981161ed447381dbbf23
divijhardwaj <divij.827@gmail.com>
2019-06-29 16:22:09 +0530

    uploaded

---
+++ src/components/SideBar.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="side-nav" :class="{'toggle-sidenav': sidenav}">
+    <div class="company-name">
+      <div class="nav-list brand">
+        <div class="list-img"></div>
+        <div class="list-label" :class="{'toggle-list-label': sidenav}">
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
+    <div class="nav-list" v-for="(item,index) in items" :key="index">
+      <div class="list-img"></div>
+      <div class="list-label" :class="{'toggle-list-label': sidenav}">
+        <p>{{sidenav}}</p>
+      </div>
+    </div>
+    <div class="close-sidenav" v-if="sidenav===true" @click="$emit('closeSidenav')"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: {
+      0: "DashBoard",
+      1: "Profile",
+      2: "EMI",
+      3: "Wallet",
+      4: "Logout"
+    }
+  }),
+  props: {
+    sidenav: false
+  }
+};
+</script>
+<style lang="scss">
+.side-nav {
+  height: 100%;
+  width: 80px;
+  //   width: 200px;
+  background-color: #1458ea;
+  color: white;
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  position: relative;
+  transition: 0.2s all ease;
+
+  &.toggle-sidenav {
+    width: 300px;
+  }
+
+  .company-name {
+    height: 120px;
+    background-color: lightblue;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .close-sidenav {
+    align-self: right;
+    width: 10px;
+    height: 10px;
+    background-color: black;
+  }
+  .nav-list {
+    width: 100%;
+    height: 60px;
+    margin-top: 10px;
+
+    background-color: lightblue;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: center;
+
+    &.brand {
+      .list-img {
+        // scale up
+        width: 40px;
+        height: 40px;
+      }
+      .list-label {
+        // scale up
+      }
+    }
+
+    .list-img {
+      display: flex;
+      width: 30px;
+      height: 30px;
+      border: 1px solid black;
+      align-items: center;
+      justify-content: left;
+    }
+    .list-label {
+      display: none;
+      flex: 2;
+      height: 30px;
+      border: 1px solid black;
+      align-items: center;
+      justiy-content: left;
+
+      &.toggle-list-label {
+        display: flex;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="main-container">
+    <div class="nav-container">
+      <input class="toggle-sidenav" @click="toggle=true" type="button">
+    </div>
+    <div class="sidenav-container">
+      <SideBar :sidenav="toggle" @closeSidenav="toggle = false"/>
+    </div>
+    <div class="content-container"></div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+<script>
+import SideBar from "../components/SideBar";
+import Component from "vue-class-component";
 
-@Component({
+export default {
   components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+    SideBar
+  },
+  data: () => ({
+    toggle: false
+  })
+};
 </script>
+
+<style lang="scss" scoped>
+.main-container {
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-areas:
+    "side-nav nav"
+    "side-nav cont";
+  grid-template-columns: 80px 1fr;
+  grid-template-rows: 80px 10fr;
+
+  .nav-container {
+    grid-area: nav;
+    position: fixed;
+    height: 80px;
+    width: 100%;
+    margin-left: 80px;
+    background-color: white;
+    box-shadow: 0px 2px 15px -15px black;
+
+    .toggle-sidenav {
+      width: 50px;
+      height: 50px;
+      background-color: black;
+      border: none;
+    }
+  }
+  .sidenav-container {
+    grid-area: side-nav;
+    position: fixed;
+    width: 80px;
+    height: 100vh;
+    // background-color: blue;
+  }
+  .content-container {
+    grid-area: cont;
+    background-color: #f8f9fb;
+    height: 1000px;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .main-container {
+    grid-template-areas:
+      "nav"
+      "cont";
+    grid-template-columns: 1fr;
+    .sidenav-container {
+      display: none;
+    }
+    .nav-container {
+      margin-left: 0%;
+    }
+  }
+}
+</style>
+ 
+
